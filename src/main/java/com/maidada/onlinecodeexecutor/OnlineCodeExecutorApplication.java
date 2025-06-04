9be6aba83f018dd181f09b61817fe25167f6cf94
maidada <1515690721@qq.com>
2025-06-04 23:53:36 +0800

    init

---
+++ src/main/java/com/maidada/onlinecodeexecutor/OnlineCodeExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.maidada.onlinecodeexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCodeExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCodeExecutorApplication.class, args);
+    }
+
+}
